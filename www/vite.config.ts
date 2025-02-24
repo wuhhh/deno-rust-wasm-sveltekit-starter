081e430893b5cb873b6bd374b169573162db8e25
Huw Roberts <huw@huwroberts.dev>
2025-02-24 15:53:46 +0000

    Add and configure vite wasm and top level await plugins

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,7 +1,9 @@
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()]
+	plugins: [sveltekit(), wasm(), topLevelAwait(), tailwindcss()]
 });
