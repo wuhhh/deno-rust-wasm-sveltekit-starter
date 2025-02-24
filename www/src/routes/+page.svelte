9e3e51a422507869fcf275cd0387b2ffeebe34b8
Huw Roberts <huw@huwroberts.dev>
2025-02-24 15:54:23 +0000

    Quick working test of imported WASM

--- www/src/routes/+page.svelte
+++ www/src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- src\routes\+page.svelte -->
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let lib: typeof import('../../../lib/rs_lib.js');  
+
+	onMount(async () => {
+		lib = await import('../../../lib/rs_lib.js');
+
+		await lib;
+
+    console.log(lib);
+		
+    const { Greeter } = lib;
+
+    // greets
+    const greeter = new Greeter("world");
+    console.log(greeter.greet());
+	});
+</script>
+
+<!--<button on:click={() => lib.greet()}>Greet</button>-->
