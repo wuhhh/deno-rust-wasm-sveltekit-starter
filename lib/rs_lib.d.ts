a3f8f3bb8b0a014da7347caffe28e6e109623152
Huw Roberts <huw@huwroberts.dev>
2025-02-24 10:40:38 +0000

    WASM scaffold

---
+++ lib/rs_lib.d.ts
@@ -0,0 +1,10 @@
+// @generated file from wasmbuild -- do not edit
+// deno-lint-ignore-file
+// deno-fmt-ignore-file
+
+export function add(a: number, b: number): number;
+export class Greeter {
+  free(): void;
+  constructor(name: string);
+  greet(): string;
+}
